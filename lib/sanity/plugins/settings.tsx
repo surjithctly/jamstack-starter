0e8dfd5d8f9b8de1ef89493a709f165bb8b44eb8
Surjith S M <surjithctly@gmail.com>
2022-12-12 14:35:42 +0530

    remove comment

--- lib/sanity/plugins/settings.tsx
+++ lib/sanity/plugins/settings.tsx
@@ -16,8 +16,7 @@ export const singletonPlugin = (types: string[]) => {
   return {
     name: "singletonPlugin",
     document: {
-      // Hide 'Singletons (such as Home)' from new document options
-      // https://user-images.githubusercontent.com/81981/195728798-e0c6cf7e-d442-4e58-af3a-8cd99d7fcc28.png
+      // Hide 'Singletons (such as Settings)' from new document options
       newDocumentOptions: (prev, { creationContext }) => {
         if (creationContext.type === "global") {
           return prev.filter(
@@ -30,7 +29,10 @@ export const singletonPlugin = (types: string[]) => {
       // Removes the "duplicate" action on the Singletons (such as Home)
       actions: (prev, { schemaType }) => {
         if (types.includes(schemaType)) {
-          return prev.filter(({ action }) => action !== "duplicate");
+          return prev.filter(
+            ({ action }) =>
+              !["unpublish", "delete", "duplicate"].includes(action)
+          );
         }
 
         return prev;
