b8cc2ae05a9f6fdd68b141e383de7b0fd313336e
Surjith S M <surjithctly@gmail.com>
2022-12-12 14:55:50 +0530

    add error if no projectid

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -5,6 +5,11 @@ import {
 } from './groq'
 import { createClient } from 'next-sanity'
 
+if (!projectId) {
+    throw Error(
+        "The Sanity Project ID is not set. Check your environment variables."
+    );
+}
 
 /**
  * Checks if it's safe to create a client instance, as `@sanity/client` will throw an error if `projectId` is false
--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -10,7 +10,7 @@ export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production" as
 
 // see https://www.sanity.io/docs/api-versioning for how versioning works
 export const apiVersion =
-    process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-12-09'
+    process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-12-10'
 
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
