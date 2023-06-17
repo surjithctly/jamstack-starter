264f5b20e4d0b27be18811f65b4d6d8a9006ea81
Surjith S M <surjithctly@gmail.com>
2023-06-17 18:48:09 +0530

    refactor: urlForImage

--- sanity/image.ts
+++ sanity/image.ts
@@ -1,7 +1,25 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import { dataset, projectId } from './config'
+import createImageUrlBuilder from "@sanity/image-url";
+import { dataset, projectId } from "./config";
 
-const imageBuilder = createImageUrlBuilder({ projectId, dataset })
+const imageBuilder = createImageUrlBuilder({ projectId, dataset });
+// image builder which sends width & height as well.
+// Also ceils maximum of 2000 width to reduce Sanity API Bandwidth
 
-export const urlForImage = (source) =>
-    imageBuilder.image(source).auto('format').fit('max')
+export const urlForImage = (source) => {
+  if (!source || !source.asset) return;
+  const dimensions = source?.asset?._ref.split("-")[2];
+
+  const [width, height] = dimensions.split("x").map((num) => parseInt(num, 10));
+
+  const url = imageBuilder
+    .image(source)
+    .auto("format")
+    .width(Math.min(width, 2000))
+    .url();
+
+  return {
+    src: url,
+    width: width,
+    height: height,
+  };
+};
